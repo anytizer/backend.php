a445b97dc2b2972338d044fd36ba70708f85d48e
Bimal Poudel <gist@bimal.org.np>
2016-05-27 15:07:35 -0600

    Post Installation database patch.

--- public_html/install/sql-scripts/data.sql
+++ public_html/install/sql-scripts/data.sql
@@ -1,7 +1,7 @@
 
 TRUNCATE `query_articles`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_articles.csv'
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_articles.csv'
 INTO TABLE `query_articles`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
@@ -9,7 +9,7 @@ LINES TERMINATED BY '\r\n';
 
 TRUNCATE `query_cdn`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_cdn.csv'
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_cdn.csv'
 INTO TABLE `query_cdn`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
@@ -17,7 +17,7 @@ LINES TERMINATED BY '\r\n';
 
 TRUNCATE `query_code_generators`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_code_generators.csv'
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_code_generators.csv'
 INTO TABLE `query_code_generators`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
@@ -25,7 +25,7 @@ LINES TERMINATED BY '\r\n';
 
 TRUNCATE `query_config`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_config.csv'
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_config.csv'
 INTO TABLE `query_config`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
@@ -33,7 +33,7 @@ LINES TERMINATED BY '\r\n';
 
 TRUNCATE `query_contacts`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_contacts.csv'
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_contacts.csv'
 INTO TABLE `query_contacts`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
@@ -41,7 +41,7 @@ LINES TERMINATED BY '\r\n';
 
 TRUNCATE `query_cruded`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_cruded.csv'
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_cruded.csv'
 INTO TABLE `query_cruded`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
@@ -49,7 +49,7 @@ LINES TERMINATED BY '\r\n';
 
 TRUNCATE `query_defines`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_defines.csv'
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_defines.csv'
 INTO TABLE `query_defines`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
@@ -57,7 +57,7 @@ LINES TERMINATED BY '\r\n';
 
 TRUNCATE `query_development_history`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_development_history.csv'
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_development_history.csv'
 INTO TABLE `query_development_history`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
@@ -65,7 +65,7 @@ LINES TERMINATED BY '\r\n';
 
 TRUNCATE `query_distributions`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_distributions.csv'
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_distributions.csv'
 INTO TABLE `query_distributions`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
@@ -73,7 +73,7 @@ LINES TERMINATED BY '\r\n';
 
 TRUNCATE `query_downloads`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_downloads.csv'
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_downloads.csv'
 INTO TABLE `query_downloads`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
@@ -81,7 +81,7 @@ LINES TERMINATED BY '\r\n';
 
 TRUNCATE `query_dropdowns`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_dropdowns.csv'
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_dropdowns.csv'
 INTO TABLE `query_dropdowns`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
@@ -89,7 +89,7 @@ LINES TERMINATED BY '\r\n';
 
 TRUNCATE `query_emails`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_emails.csv'
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_emails.csv'
 INTO TABLE `query_emails`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
@@ -97,7 +97,7 @@ LINES TERMINATED BY '\r\n';
 
 TRUNCATE `query_emails_smtp`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_emails_smtp.csv'
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_emails_smtp.csv'
 INTO TABLE `query_emails_smtp`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
@@ -105,7 +105,7 @@ LINES TERMINATED BY '\r\n';
 
 TRUNCATE `query_errors`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_errors.csv' 
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_errors.csv' 
 INTO TABLE `query_errors`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"' 
 LINES TERMINATED BY '\r\n';
@@ -113,7 +113,7 @@ LINES TERMINATED BY '\r\n';
 
 TRUNCATE `query_identifiers`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_identifiers.csv'
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_identifiers.csv'
 INTO TABLE `query_identifiers`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
@@ -121,7 +121,7 @@ LINES TERMINATED BY '\r\n';
 
 TRUNCATE `query_licenses`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_licenses.csv'
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_licenses.csv'
 INTO TABLE `query_licenses`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
@@ -129,7 +129,7 @@ LINES TERMINATED BY '\r\n';
 
 TRUNCATE `query_logger`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_logger.csv'
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_logger.csv'
 INTO TABLE `query_logger`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
@@ -137,7 +137,7 @@ LINES TERMINATED BY '\r\n';
 
 TRUNCATE `query_menus`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_menus.csv'
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_menus.csv'
 INTO TABLE `query_menus`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
@@ -145,7 +145,7 @@ LINES TERMINATED BY '\r\n';
 
 TRUNCATE `query_messages`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_messages.csv'
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_messages.csv'
 INTO TABLE `query_messages`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
@@ -153,7 +153,7 @@ LINES TERMINATED BY '\r\n';
 
 TRUNCATE `query_pages`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_pages.csv'
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_pages.csv'
 INTO TABLE `query_pages`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
@@ -161,7 +161,7 @@ LINES TERMINATED BY '\r\n';
 
 TRUNCATE `query_server`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_server.csv'
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_server.csv'
 INTO TABLE `query_server`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
@@ -169,7 +169,7 @@ LINES TERMINATED BY '\r\n';
 
 TRUNCATE `query_sessions`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_sessions.csv'
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_sessions.csv'
 INTO TABLE `query_sessions`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
@@ -177,7 +177,7 @@ LINES TERMINATED BY '\r\n';
 
 TRUNCATE `query_subdomains`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_subdomains.csv'
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_subdomains.csv'
 INTO TABLE `query_subdomains`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
@@ -185,7 +185,7 @@ LINES TERMINATED BY '\r\n';
 
 TRUNCATE `query_subdomains_categories`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_subdomains_categories.csv'
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_subdomains_categories.csv'
 INTO TABLE `query_subdomains_categories`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
@@ -193,7 +193,7 @@ LINES TERMINATED BY '\r\n';
 
 TRUNCATE `query_subdomains_status`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_subdomains_status.csv'
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_subdomains_status.csv'
 INTO TABLE `query_subdomains_status`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
@@ -201,7 +201,7 @@ LINES TERMINATED BY '\r\n';
 
 TRUNCATE `query_tables`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_tables.csv'
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_tables.csv'
 INTO TABLE `query_tables`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
@@ -209,7 +209,7 @@ LINES TERMINATED BY '\r\n';
 
 TRUNCATE `query_toc`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_toc.csv' 
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_toc.csv' 
 INTO TABLE `query_toc`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"' 
 LINES TERMINATED BY '\r\n';
@@ -217,7 +217,7 @@ LINES TERMINATED BY '\r\n';
 
 TRUNCATE `query_uploads`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_uploads.csv'
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_uploads.csv'
 INTO TABLE `query_uploads`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
@@ -225,7 +225,7 @@ LINES TERMINATED BY '\r\n';
 
 TRUNCATE `query_users`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_users.csv'
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_users.csv'
 INTO TABLE `query_users`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
@@ -233,7 +233,7 @@ LINES TERMINATED BY '\r\n';
 
 TRUNCATE `query_users_groups`;
 
-LOAD DATA LOCAL INFILE '../public_html/install/sql-scripts/_csv/query_users_groups.csv'
+LOAD DATA LOCAL INFILE '../../public_html/install/sql-scripts/_csv/query_users_groups.csv'
 INTO TABLE `query_users_groups`
 FIELDS ESCAPED BY '\\' TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
