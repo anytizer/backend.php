7408d94a8b838b237ba5bcd75be90b6d1fcca58b
bimal <gist@bimal.org.np>
2017-01-08 17:30:03 -0700

    auto documentation with apigen

--- apigen/apigen.bat
+++ apigen/apigen.bat
@@ -1,3 +1,6 @@
 @echo off
-curl -L -O https://github.com/ApiGen/ApiGen.github.io/raw/master/apigen.phar
-php apigen.phar generate -s ../library -d ../documentation
+
+REM If running for the first time only
+REM curl -L -O https://github.com/ApiGen/ApiGen.github.io/raw/master/apigen.phar
+
+php apigen.phar generate -s ../library --skip-doc-path="~/vendors" --no-source-code -d ../documentation
