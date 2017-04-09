ae1237251287d7220320191a6774622adda41b7b
bimal <gist@bimal.org.np>
2017-04-08 20:57:19 -0600

    name - touched.

--- library/common/class.names.inc.php
+++ library/common/class.names.inc.php
@@ -16,12 +16,12 @@ class names
      */
     public function read_name($full_name = "", $fix_case = false)
     {
-        $names = explode(' ', $full_name);
+        $names = explode(" ", $full_name);
 
         $name = array();
-        $name['F'] = "";
-        $name['M'] = "";
-        $name['L'] = "";
+        $name["F"] = "";
+        $name["M"] = "";
+        $name["L"] = "";
         # Assigning these empty values makes sure that each of the three parts exists.
 
         $words = count($names);
@@ -29,34 +29,36 @@ class names
             case 0:
                 break;
             case 1:
-                $name['F'] = $names[0];
+                $name["F"] = $names[0];
                 break;
             case 2:
-                $name['F'] = $names[0];
-                $name['L'] = $names[1];
+                $name["F"] = $names[0];
+                $name["L"] = $names[1];
                 break;
             case 3:
-                $name['F'] = $names[0];
-                $name['M'] = $names[1];
-                $name['L'] = $names[2];
+                $name["F"] = $names[0];
+                $name["M"] = $names[1];
+                $name["L"] = $names[2];
                 break;
             default:
                 # Case of 4 or more than 4 words in the name
 
                 # First and last names are predictable
-                $name['F'] = $names[0];
-                $name['L'] = $names[$words - 1];
+                $name["F"] = $names[0];
+                $name["L"] = $names[$words - 1];
 
                 # Now consider the words between
                 $name_temp = $names;
                 unset($name_temp[0]);
                 unset($name_temp[$words - 1]);
-                $name['M'] = implode(' ', $name_temp);
+                $name["M"] = implode(" ", $name_temp);
+				
+				break;
         }
 
         if ($fix_case === true) {
-            $name = array_map('strtolower', $name);
-            $name = array_map('ucfirst', $name);
+            $name = array_map("strtolower", $name);
+            $name = array_map("ucfirst", $name);
         }
 
         return $name;
